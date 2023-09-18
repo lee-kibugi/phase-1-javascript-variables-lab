1009fe1ef560ae5538f4059fe4293df96a7de335
linda wanjiku <lindashykooh@gmail.com>
2023-09-18 18:06:23 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+
+
+let companyName = "Scuber";
+function companyName(){
+    return 'is set as Scuber';
+}
+
+// const companyName = "Scuber";
+// function companyName(){
+//     return 'is set as Scuber';
+// }
+
+let mostProfitableNeighborhood =  "Chelsea"
+function mostProfitableNeighborhood(){
+return 'is declared as equal to Chelsea';
+}
+
+let companyCeo ='Susan Smith'
+function companyCeo(){
+return 'is declared as equal to Susan Smith'
+}
